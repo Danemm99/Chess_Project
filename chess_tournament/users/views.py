19f6d829fb79645666f2ef6cf2ef249b93dad504
Danyil Zlobin <alice@programmingmentor.com>
2023-12-11 10:43:36 -0500

    class-based-views

--- chess_tournament/users/views.py
+++ chess_tournament/users/views.py
@@ -1,66 +1,80 @@
 from django.shortcuts import render, redirect
+from django.views import View
 from .forms import RegistrationForm, LoginForm
 from .models import CustomUser
 from django.contrib.auth.hashers import check_password
 
 
-def register(request):
-    if request.method == 'POST':
+class RegisterView(View):
+    template_name = 'registration/register.html'
+
+    def get(self, request):
+        form = RegistrationForm()
+        return render(request, self.template_name, {'form': form})
+
+    def post(self, request):
         form = RegistrationForm(request.POST)
         if form.is_valid():
             form.save()
             return redirect('login')
-    else:
-        form = RegistrationForm()
-    return render(request, 'registration/register.html', {'form': form})
+        return render(request, self.template_name, {'form': form})
 
 
-def login_view(request):
-    if CustomUser.objects.filter(is_active=True).exists():
-        return redirect('home')
+class LoginView(View):
+    template_name = 'login/login.html'
 
-    if request.method == 'POST':
+    def get(self, request):
+        if CustomUser.objects.filter(is_active=True).exists():
+            return redirect('home')
+        form = LoginForm()
+        return render(request, self.template_name, {'form': form})
+
+    def post(self, request):
         form = LoginForm(request.POST)
         if form.is_valid():
             email = form.cleaned_data['email']
             password = form.cleaned_data['password']
             user = CustomUser.get_by_email(email)
-            print(user)
             if user and check_password(password, user.password):
                 request.session['user_id'] = user.user_id
                 CustomUser.objects.filter(user_id=user.user_id).update(is_active=True)
                 return redirect('home')
             else:
-                return render(request, 'login/login.html', {'form': form, 'error': 'Invalid email or password.'})
-    else:
-        form = LoginForm()
-    return render(request, 'login/login.html', {'form': form})
+                return render(request, self.template_name, {'form': form, 'error': 'Invalid email or password.'})
+        return render(request, self.template_name, {'form': form})
 
 
-def logout_view(request):
-    if 'user_id' in request.session:
+class LogoutView(View):
+    @staticmethod
+    def get(request):
+        if 'user_id' in request.session:
+            user_id = request.session.get('user_id')
+            CustomUser.objects.filter(user_id=user_id).update(is_active=False)
+            del request.session['user_id']
+        return redirect('main')
+
+
+class HomeView(View):
+    template_name_participant = 'participant/participant.html'
+    template_name_coach = 'coach/coach.html'
+
+    def get(self, request):
         user_id = request.session.get('user_id')
-        CustomUser.objects.filter(user_id=user_id).update(is_active=False)
-        del request.session['user_id']
-    return redirect('main')
+        role = CustomUser.get_by_id(user_id).role
+        context = {}
+
+        if user_id:
+            user = CustomUser.get_by_id(user_id)
+            context['user'] = user
+
+        if user_id and role == 'participant':
+            return render(request, self.template_name_participant, context)
+        else:
+            return render(request, self.template_name_coach, context)
 
 
-def home(request):
-    user_id = request.session.get('user_id')
-    role = CustomUser.get_by_id(user_id).role
-    context = {}
+class MainView(View):
+    template_name = 'main/main.html'
 
-    if user_id:
-        user = CustomUser.get_by_id(user_id)
-        context['user'] = user
-
-    if role == 'participant':
-        return render(request, 'participant/participant.html', context)
-
-    else:
-        return render(request, 'coach/coach.html', context)
-
-
-def main(request):
-    return render(request, 'main/main.html')
-
+    def get(self, request):
+        return render(request, self.template_name)
